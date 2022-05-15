1ca868e00f19a4c26cbd2f882c12a6cbab723659
JdToit <jeandu.toit3@yahoo.com>
2022-05-15 12:08:46 -0700

    learning about git commit

---
+++ dictionaries.py
@@ -0,0 +1,5 @@
+# This is how we create a dictionary
+
+dic_1 = {'Names': ['Jack', 'Sun', 'Su', 'Achmed', 'Sven'], 
+         'Emails': ['jack123@gmail.com', 'sun@yahoo.com', 'su5@work.org', 'achmed55@gmail', 'sven77@yahoo.com'],
+         'Passwords': ['123', '7a7', '11ab', '12tv', '6551']}
